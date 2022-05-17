9d780f90ddc85623f41e2ed5f91fe1f25f669c70
selvan <p.thamarai@gmail.com>
2022-05-17 17:35:02 +0530

    Webview with WindowManager

--- app/src/main/java/com/elyeproj/windowmanager/FloatView.kt
+++ app/src/main/java/com/elyeproj/windowmanager/FloatView.kt
@@ -6,13 +6,16 @@ import android.util.AttributeSet
 import android.view.Gravity
 import android.view.MotionEvent
 import android.view.WindowManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.LinearLayout
 import androidx.appcompat.widget.AppCompatImageView
 
 abstract class FloatView (
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : AppCompatImageView(context, attrs, defStyleAttr) {
+) : WebView(context, attrs, defStyleAttr) {
 
     private val windowManager by lazy {
         context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
@@ -25,9 +28,17 @@ abstract class FloatView (
     abstract val type: Int
 
     init {
+        webViewClient = WebViewClient()
+        layoutParams =
+            LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT)
+        settings.javaScriptEnabled = true
+        loadUrl("https://flutter.github.io/samples/web/github_dataviz/")
+
         val paramFloat = WindowManager.LayoutParams(
-            WindowManager.LayoutParams.WRAP_CONTENT,
-            WindowManager.LayoutParams.WRAP_CONTENT,
+            640,
+            480,
             type,
             WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
             PixelFormat.TRANSLUCENT)
