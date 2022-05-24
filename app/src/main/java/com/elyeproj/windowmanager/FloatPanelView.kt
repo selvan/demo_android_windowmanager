a54e3db01e65f92feb41e40d6787783fa1a6cc25
selvan <p.thamarai@gmail.com>
2022-05-24 21:07:07 +0530

    Latest

--- app/src/main/java/com/elyeproj/windowmanager/FloatPanelView.kt
+++ app/src/main/java/com/elyeproj/windowmanager/FloatPanelView.kt
@@ -2,6 +2,7 @@ package com.elyeproj.windowmanager
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.WindowManager
 
 class FloatPanelView @JvmOverloads constructor(
@@ -10,6 +11,26 @@ class FloatPanelView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : FloatView(context, attrs, defStyleAttr) {
 
+    var zIndex =0
+    init {
+        Log.d("FloatPanelView", "x is ${ZIndex.x}")
+        zIndex = ZIndex.current()
+    }
+
     override val type: Int
-        get() = WindowManager.LayoutParams.TYPE_APPLICATION_PANEL
+        // TYPE_APPLICATION_SUB_PANEL is always on top of TYPE_APPLICATION_PANEL
+        get() = if(ZIndex.current()==1) WindowManager.LayoutParams.TYPE_APPLICATION_SUB_PANEL else WindowManager.LayoutParams.TYPE_APPLICATION_PANEL
+
+}
+
+object ZIndex{
+    var x = 0
+
+    fun current(): Int {
+        synchronized(this) {
+            x += 1
+            return x
+        }
+    }
+
 }
